cefbba549906b5d15e1ff5c02daa8ae741b08340
Your GitHub Username <anaparthisandeep02@gmail.com>
2025-04-09 14:59:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll to section smoothly when nav link is clicked
+document.querySelectorAll('a[href^=\"#\"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
